0a3ed8b7e85b3da645c4b7d7eb0caa11d22a32cd
ohadar <ohadar@kramerav.com>
2023-02-14 21:04:18 +0200

    Edit item price problem fixed

--- src/components/ShoppingItemEdit.vue
+++ src/components/ShoppingItemEdit.vue
@@ -6,21 +6,13 @@
         <form v-if="shoppingItemToEdit" @submit.prevent="save" class="item-edit-form">
           <div class="item-edit-form__inputs">
             <input class="item-edit-form__input" type="text" placeholder="Name" v-model="shoppingItemToEdit.name"/>
-            <input class="item-edit-form__input" type="price" placeholder="Price" v-model="shoppingItemToEdit.price"/>
+            <input class="item-edit-form__input" type="price" placeholder="Price" v-model.number="shoppingItemToEdit.price"/>
           </div>
-          <!--        <label>-->
-          <!--          Attach to labels?-->
-          <!--          <el-select v-model="shoppingItemToEdit.labels">-->
-          <!--            <el-option value="Fruits">Fruits</el-option>-->
-          <!--            <el-option value="Milk">Milk</el-option>-->
-          <!--          </el-select>-->
-          <!--        </label>-->
           <el-select v-model="shoppingItemToEdit.labels" multiple>
             <el-option value="Fruits">Fruits</el-option>
             <el-option value="Milk">Milk</el-option>
             <el-option value="Food">Food</el-option>
             <el-option value="Furniture">Furniture</el-option>
-            <el-option value="Doll">Doll</el-option>
             <el-option value="Carbohydrates">Carbohydrates</el-option>
           </el-select>
           <textarea class="item-edit-form__textarea" type="text" placeholder="Description" v-model="shoppingItemToEdit.description"/>
